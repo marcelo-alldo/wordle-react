d81b72dddc5d62d3fbf5521121a17013ff34f67f
Crhistian <crhistiandeoliveira@gmail.com>
2022-02-04 09:08:01 -0300

    mostra como funciona o gitflow

--- src/pages/home/Home.js
+++ src/pages/home/Home.js
@@ -1,13 +1,13 @@
 import { Grid } from '@mui/material';
 
 function Home() {
-    return (
-        <Grid container>
-            <Grid item xs={12}>
-                <h1>Wordle React</h1>
-            </Grid>
-        </Grid>
-    );
+  return (
+    <Grid container>
+      <Grid item xs={12}>
+        <h1>Wordle React</h1>
+      </Grid>
+    </Grid>
+  );
 }
 
 export default Home;
