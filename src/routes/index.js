8f15db947f9359fbc70f2dff1aaffa188c576bdd
Marcelo Eltz <marcelo@growdev.com.br>
2022-02-04 17:02:55 -0300

    Adicionado uma nova pagina, about e um component de title (tilte-page)

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,12 +1,15 @@
 import { Routes, Route } from 'react-router-dom';
 
 import Home from '../pages/home/Home';
+import About from '../pages/about/About';
 
 export default function RoutesApp() {
-    return (
-        <Routes>
-            <Route path='/' element={<Home />} />
-            <Route path='home' element={<Home />} />
-        </Routes>
-    );
+  return (
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="home" element={<Home />} />
+      <Route path="about" element={<About />} />
+      <Route path="*" element={<Home />} />
+    </Routes>
+  );
 }
